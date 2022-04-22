a1a1722e2028afb65f720f35fca35bc2d2c27878
Rodrigo Valdelomar <rodrigo.valdelomar@gmail.com>
2022-04-22 18:15:06 +0200

    first commit

--- page/models.py
+++ page/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Pomodoro(models.Model):
+    excercise   = models.IntegerField()
+    rest        = models.IntegerField()
+    sets        = models.IntegerField()
