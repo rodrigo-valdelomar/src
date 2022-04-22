715f6299813ba5b5d15e3c6caaa9de8ba539072e
Rodrigo Valdelomar <rodrigo.valdelomar@gmail.com>
2022-04-22 18:53:04 +0200

    Created pomodoro create view

--- page/views.py
+++ page/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 
+from .forms import PomodoroForm
 # Create your views here.
+
+def pomodoro_create_view(request):
+    form = PomodoroForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+
+    context = {
+        "form" : form
+    }
+
+    return render(request, "page/pomodoro_create.html", context)
 
 # tomar los tiempos
 
 # hacer correr el tiempo
 
 # avisar que se cumplió el tiempo
-
