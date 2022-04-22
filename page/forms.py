4bce8d732e94f054ca457903eabdafcd55d9bc03
Rodrigo Valdelomar <rodrigo.valdelomar@gmail.com>
2022-04-22 18:52:45 +0200

    Create model form

--- page/forms.py
+++ page/forms.py
@@ -1,2 +1,11 @@
 from django import forms
 from . import Pomodoro
+
+class PomodoroForm(forms.ModelForm):
+    
+    class Meta:
+        model = Pomodoro
+        fields = [
+            "excercise",
+            "rest",
+            "sets"]
